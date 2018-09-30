7abd3233b1dbaf235dcf7e1c443199f5a6991a09
Hithaishy Bajje <Hithaishy.jc@gmail.com>
2018-09-30 14:24:50 -0700

    setup the weather api and from

--- src/App.js
+++ src/App.js
@@ -5,22 +5,61 @@ import Weather from "./components/Weather"
 
 const API_KEY = "d3df97f9890cae03f5ff3203916e31bd";
 
-class App extends React.Component{
+class App extends React.Component {
 
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=California,Us&appid=${API_KEY}&units=metric`);
+  state = {
+    temprature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: ""
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}s&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    if (city && country) {
+      this.setState({
+        temprature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        err: ""
+      });
+    } else {
+      this.setState({
+        temprature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value"
+      });
+    }
   }
   render() {
     return (
       <div>
         <Title />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temprature={this.state.temprature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
-}
+};
+
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                <p> Weather component</p>
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+                { this.props.temprature  && <p>Temprature:{this.props.temprature}</p>}
+                { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                { this.props.description && <p>Conditions: {this.props.description}</p> }
+                { this.props.error && <p>{ this.props.error }</p> }
             </div>
         );
     }
